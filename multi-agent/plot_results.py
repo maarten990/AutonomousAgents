bc25595f5a9bcd33f568ef9cde7f25822f2f10a8
Ties van Rozendaal <git@tivaro.nl>
2014-12-12 18:35:51 +0100

    Plotterrrrtje

--- multi-agent/plot_results.py
+++ multi-agent/plot_results.py
@@ -5,22 +5,22 @@ import matplotlib.pyplot as plt
 def main():
 
 	#single_setting_line_plot(var_value=0.2)
+	num_preds_plot()
+	num_preds_plot(plotWinners = True)
 
-	var_effect_line_plot(title='Independen Q learning with 2 predators',changevar='alpha',var_values=[0.1, 0.2, 0.3, 0.4, 0.5])
-	var_effect_line_plot(title='Independen Q learning with 2 predators',changevar='alpha',var_values=[0.1, 0.2, 0.3, 0.4, 0.5],plotWinners=True)
+	var_effect_line_plot(title='Independent Q learning with 2 predators',changevar='alpha',var_values=[0.1, 0.2, 0.3, 0.4, 0.5])
+	var_effect_line_plot(title='Independent Q learning with 2 predators',changevar='alpha',var_values=[0.1, 0.2, 0.3, 0.4, 0.5],plotWinners=True)
 
-	var_effect_line_plot(title='Independen Q learning with 2 predators',changevar='epsilon',var_values=[0,0.1,0.5,0.9])
-	var_effect_line_plot(title='Independen Q learning with 2 predators',changevar='epsilon',var_values=[0,0.1,0.5,0.9],plotWinners=True)	
+	var_effect_line_plot(title='Independent Q learning with 2 predators',changevar='epsilon',var_values=[0,0.1,0.5,0.9])
+	var_effect_line_plot(title='Independent Q learning with 2 predators',changevar='epsilon',var_values=[0,0.1,0.5,0.9],plotWinners=True)	
 
-	var_effect_line_plot(title='Independen Q learning with 2 predators',changevar='gamma',var_values=[0.1, 0.5, 0.7, 0.9])
-	var_effect_line_plot(title='Independen Q learning with 2 predators',changevar='gamma',var_values=[0.1, 0.5, 0.7, 0.9],plotWinners=True)
+	var_effect_line_plot(title='Independent Q learning with 2 predators',changevar='gamma',var_values=[0.1, 0.5, 0.7, 0.9])
+	var_effect_line_plot(title='Independent Q learning with 2 predators',changevar='gamma',var_values=[0.1, 0.5, 0.7, 0.9],plotWinners=True)
 
-	var_effect_line_plot(title='Independen Q learning with 2 predators',changevar='initial_value',greekVar=False,var_values=[0, 5, 15, 50])
-	var_effect_line_plot(title='Independen Q learning with 2 predators',schangevar='initial_value',greekVar=False,var_values=[0, 5, 15, 50],plotWinners=True)
+	var_effect_line_plot(title='Independent Q learning with 2 predators',changevar='initial_value',greekVar=False,var_values=[0, 5, 15, 50])
+	var_effect_line_plot(title='Independent Q learning with 2 predators',schangevar='initial_value',greekVar=False,var_values=[0, 5, 15, 50],plotWinners=True)
 
 
-
-	
 
 
 	var_effect_line_plot()		
@@ -108,6 +108,41 @@ def var_effect_line_plot(algorithm="Q",num_pred =2,changevar="alpha",var_values=
 	plt.legend()
 	plt.xlabel('Episode')
 	plt.show()
+
+def num_preds_plot(algorithm="Q",num_preds=[1,2,3,4],plotWinners=False):
+	"""
+	Plots the episode on the x axis
+	On the y axis: Steps if plotWinners = False, else proportion won by preds
+	Plots different colored lines for different num predators
+	"""
+
+	plt.hold(True)
+	plt.grid(b=True, which='both', color='0.65',linestyle='-')
+
+	for num_pred in num_preds:
+
+		winners_mat, steps_mat = load_results(algorithm,num_pred,'alpha',0.5)
+
+		if plotWinners:
+			mat = winners_mat
+			plt.ylabel('Proportion of games won by the predators')
+		else:
+			mat = steps_mat
+			plt.ylabel('Number of steps untill game ends')
+
+		if mat.any():
+
+			yvar = np.average(mat,axis=0)
+
+			xs = range(len(yvar))
+
+			plt.plot(xs, yvar, label='$Number of predators: {0}$'.format(num_pred))
+
+	plt.title('Independent Q learning')
+	plt.legend()
+	plt.xlabel('Episode')
+	plt.show()
+
 
 
 def plot_winner_scatter(steps,winners):
